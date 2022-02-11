7e1ab715020d81164a3538c63b20face797e1b77
naga <na@gc.com>
2022-02-11 22:44:16 +0530

    thisis a commit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,13 +9,13 @@
 
     function initialiseProgram($scope, $filter){
         $scope.LuchList='';
-        // $scope.LunchNumber=0;
         $scope.LunchMsg='';
 
 
         $scope.CheckMyLunch = function(){
         $scope.LunchNumber=0;
            if(!$scope.LuchList){
+            $scope.msgcolor='red';
             $scope.LunchMsg = 'Please enter data first';
                return;
            }
@@ -24,7 +24,8 @@
 
             for(var checkVal=0; checkVal<item.length; checkVal++){
                 if(item[checkVal]==''){
-                    $scope.LunchMsg='we do NOT consider and empty item';
+                    $scope.msgcolor='red';
+                    $scope.LunchMsg='we do NOT consider an empty item';
                     return;
                 }else{
                    $scope.LunchNumber++;
@@ -33,9 +34,17 @@
             }
 
             if($scope.LunchNumber<=3){
+                $scope.msgcolor='green';
                 $scope.LunchMsg='Enjoy!';
             }else{
+                $scope.msgcolor='green';
                 $scope.LunchMsg='Too much!';
+            }
+
+            $scope.ClearMyLunch = function(){
+                $scope.LuchList='';
+                $scope.LunchMsg='';
+                $scope.msgcolor='';
             }
 
         }
